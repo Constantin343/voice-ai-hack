1562337f40455a370165155789c055fa74ccfabf
Constantin Weberpals <constantin.weberpals@tum.de>
2024-12-03 23:52:24 -0800

    fix screen order

--- src/components/onboarding/onboarding-screen.tsx
+++ src/components/onboarding/onboarding-screen.tsx
@@ -32,14 +32,12 @@ export default function OnboardingScreen() {
 
     const screens = [
         <DataProcessingScreen onNext={goToNextStep}/>,
-        <PersonaCreationScreen onNext={goToNextStep}/>,
-        <IntroVideoScreen onNext={goToNextStep}/>,
-        <DataProcessingScreen onNext={goToNextStep}/>,
-        
-        <SummaryScreen onNext={() => console.log("Onboarding completed!")}/>,
 
         <RequestLinkedinScreen onLinkedInSubmit={handleLinkedInScraping} onNext={goToNextStep}/>,
-       
+        <IntroVideoScreen onNext={goToNextStep}/>,
+        <DataProcessingScreen onNext={goToNextStep}/>,
+        <PersonaCreationScreen onNext={goToNextStep}/>,
+        <SummaryScreen onNext={() => console.log("Onboarding completed!")}/>
     ];
 
     return (
